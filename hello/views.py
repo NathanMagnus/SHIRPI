852757988e5d8e6250528efaeef5ad2d284dbda0
Nathan <ddragonsdd@gmail.com>
2009-10-01 01:16:33 -0400

    -static files working -admin page created - no style

--- hello/views.py
+++ hello/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render_to_response
 
 def index(request):
 	return render_to_response('hello/index.html')
+def style(request):
+	return render_to_response('hello/style.css')
