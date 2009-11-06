b67e0fe52fc3dc3d92889ec3eed72bf15c0324ad
Nathan <ddragonsdd@gmail.com>
2009-11-06 15:29:12 -0500

    Start changing to divs

--- SHIRPI/templatetags/browse_filters.py
+++ SHIRPI/templatetags/browse_filters.py
@@ -4,25 +4,9 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-#display a group of restaurants in a table
-@register.filter
-def display_restaurant_set(set, category, autoescape=None):
-	#start table
-	result = "<table class=\"" + category + " restaurant_set\">"
-	result += "<caption>" + category.capitalize() + "</caption>"
-	result += "<tr>" + display_restaurant_table_header(set) + "</tr>"
-	#display info for each restaurant in the set
-	for rest in set:
-		result += "<tr>" + display_restaurant_info(rest) + "</tr>"
-	#end table and return resulting table
-	result += "</table>"
-	return mark_safe(result)
-display_restaurant_set.needs_autoescape = True
-display_restaurant_set.needs_category = True
-
 #display the info of one restaurant
 @register.filter
-def display_restaurant_info(value, autoescape=None):
+def display_restaurant(value, autoescape=None):
 	if value.cleanliness_count<1:
 		value.cleanliness_count=1
 	if value.combined_count<1:
@@ -33,32 +17,13 @@ def display_restaurant_info(value, autoescape=None):
 		value.atmosphere_count = 1
 	if value.wait_time_count<1:
 		value.wait_time_count =1 
-	result = '\n\t<td><a href="/cs215/shirpi/browse/' + value.name +'/' + value.address + '">' + value.name + '</a></td>'
-	result += '<td>' + str(value.address) + '</td><td>' + str(value.health_report_status) +'</td>'
+	result = "<h3 class='name'><a href='/cs215/shirpi/browse/" + value.name +"/" + value.address + "'>" + value.name + "</a></h3>"
+	result +="<h3 class='address'>" + str(value.address) + "</h3>"
+	result += "<div class='health_report_status'>" + str(value.health_report_status) +"</div>"
 	result += '<td>' + str(value.combined/value.combined_count) + '</td><td>' + str(value.food_quality/value.food_quality_count) + '</td>'
 	result += '<td>' + str(value.cleanliness/value.cleanliness_count) + '</td><td>' + str(value.atmosphere/value.atmosphere_count) + '</td><td>' + str(value.wait_time/value.wait_time_count) + '</td>'
 	return mark_safe(result)
 display_restaurant_info.needs_autoescape = True
-
-#display a group of comments in a table
-@register.filter
-def display_comment_set(set, user_name, autoescape=None):
-	#start the table
-	result = ""
-	
-	result += "<div id='comment_set'>\n"
-	result += "<h2>Comments</h2>\n"
-	#result += <tr>"" + display_comment_table_header(set, user_name) + "</tr>\n"
-	
-	#put the information for each comment into the table
-	for comment in set:
-		result += "<div class='comment'>\n" + display_comment(comment, user_name) + "</	div>\n"
-	#end the table and return it
-	result += "</div>"
-	
-	return mark_safe(result)
-display_comment_set.needs_autoescape=True
-display_comment_set.needs_user_name = True
 
 #display one comment
 @register.filter
@@ -98,18 +63,3 @@ def display_comment(value, user_name,autoescape=None):
 	return mark_safe(result)
 display_comment.needs_autoescape = True
 display_comment.needs_user_name = True
-
-#display restaurant title row
-@register.filter
-def display_restaurant_table_header(value, autoescape=None):
-	result =  '<th>Name</th><th width="200px">Address</th><th>HI Status</th><th>Combined</th><th>Food Quality</th><th>Cleanliness</th><th>Atmosphere</th><th>Wait Time</th>'
-	return mark_safe(result)
-display_restaurant_table_header.needs_autoescape = True
-
-#display comment title row
-@register.filter
-def display_comment_table_header(value, autoescape=None):
-	result = '<th>Author</th><th width="300px">Comment</th><th>Combined</th><th>Food Quality</th><th>Cleanliness</th><th>Atmosphere</th><th>Wait Time</th>'
-	return mark_safe(result)
-display_comment_table_header.needs_autoescape = True
-
