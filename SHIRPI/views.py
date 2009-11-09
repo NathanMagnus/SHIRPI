039f32e6a13302ddf567feb9475771304ae209ae
Nathan <ddragonsdd@gmail.com>
2009-11-08 19:07:05 -0500

    lower case 'all' after typing errors

--- SHIRPI/views.py
+++ SHIRPI/views.py
@@ -26,7 +26,7 @@ def index(request):
 #browsing restaurants
 def browse(request, restaurant_name, restaurant_address):
 	#if they inputted "All" for the restaurant name and address
-	if lowercase(restaurant_name)=="all" and lowercase(restaurant_address)=="all":
+	if restaurant_name.lowercase()=="all" and restaurant_address.lowercase=="all":
 		#find all critical, moderate and good restauratns
 		Critical = Restaurant.objects.filter(health_report_status__gte=CRITICAL_VAL).order_by("-health_report_status")
 		Moderate = Restaurant.objects.filter(health_report_status__lt=CRITICAL_VAL).filter(health_report_status__gte=MODERATE_VAL).order_by("-health_report_status")
@@ -34,13 +34,13 @@ def browse(request, restaurant_name, restaurant_address):
 		#render the response
 		return render_to_response("SHIRPI/browse.html", {'Critical':Critical, 'Moderate':Moderate, 'Good': Good, 'user':request.user})
 	#if they want all restaurants with a certain address
-	elif lowercase(restaurant_name)=="all":
+	elif restaurant_name.lowercase=="all":
 		try:
 			chain = Restaurant.objects.filter(address__contains=restaurant_address)
 		except Restaurant.DoesNotExist:
 			error = "Restaurant with that address does not exist"
 	#if they want a restaurant based on name
-	elif lowercase(restaurant_address)=="all":
+	elif restaurant_address.lowercase=="all":
 		try:
 			chain = Restaurant.objects.filter(name__contains=restaurant_name)
 		except Restaurant.DoesNotExist:
