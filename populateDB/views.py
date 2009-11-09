1cd5ea14b43d862cf7513d4a42f01acefd9a9a95
Nathan <ddragonsdd@gmail.com>
2009-11-09 15:04:37 -0500

    start work on redoing populate descriptions

--- populateDB/views.py
+++ populateDB/views.py
@@ -9,80 +9,80 @@ def populateMaster():
 	# This section needs to be separate and with further admin panel accessibility.
 	# Further, it needs an overhaul to allow for more content.
 	# Kyle is all talk. DO IT don't complain about it
-	items = {
-		'1': {'short_description': 'Improper storage of perishable foods', 
+	items = [
+		{'short_description': 'Improper storage of perishable foods', 
 			'description': "Potentially hazardous foods and perishable foods must be stored at 4oC/40oF or below Hazardous foods must be thawed in a refrigerator or under cold, running water.",
 			'severity': 1
 			},
-		'2': {'short_description': 'Temperatures', 
+		{'short_description': 'Temperatures', 
 			'description': 'Temperatures Cook foods to an internal temperature of: a) 63oC (145oF) or above for: eggs (if prepared for immediate service); medium rare beef and veal steaks and roasts; b) 68oC (155oF) or above for: game farm meat products; c) 70oC (158oF) for: fish; d) 71oC (160oF) or above for: ground beef/pork/veal; food made with ground beef/pork/veal, e.g. sausages, meatballs; pork chops, ribs and roasts; e) 74oC (165oF) or above for: ground chicken/turkey; food made with ground chicken/turkey or mixtures containing poultry, meat, fish, or eggs; chicken and turkey breasts, legs, thighs and wings; stuffing (inside a carcass); stuffed pasta; hot dogs; leftovers; egg dishes (if not prepared as specified in 2a); and stuffed fish; f) 85oC (185oC) or above for: chicken and turkey, whole bird. Reheat foods rapidly to an internal temperature of 74oC (165oF) prior to serving. Hot Holding must maintain an internal temperature of 60oC (140oF) or higher.',
 			'severity': 1
 			},
-		'3': {'short_description': 'Storage Containers',
+		{'short_description': 'Storage Containers',
 			'description': 'Foods must be stored in food grade containers, properly labeled and protected from contamination at all times.',
 			'severity': 1
 			},
-		'4': {'short_description': 'Hand Washing',
+		{'short_description': 'Hand Washing',
 			'description': 'Hand washing must be properly done at appropriate times and intervals. An accessible, plumbed hand basin with hot and cold running water, soap in a dispenser and single-use paper towels in wall-mounted dispensers are required in food preparation areas. Hand washing Procedure: a) Wet hands and exposed arms (at least up to wrist) with warm running water; b) Apply liquid soap; c) Vigorously rub together wet surfaces for at least 20 seconds, lathering at least up to wrist; d) Use a nailbrush under fingernails and other very dirty areas; e) Thoroughly rinse with clean, warm water running from wrists to fingertips; f) Apply soap and lather vigorously again; g) Rinse hands and wrists thoroughly; h) Dry hands with a single-use paper towel; and i) Use paper towel to turn off tap.',
 			'severity': 1
 			},
-		'5': {'short_description': 'Employee Hygiene',
+		{'short_description': 'Employee Hygiene',
 			'description': 'Good personal hygiene must be practiced at all times. Food handlers with infectious or contagious diseases (or symptoms) should not work. ',
 			'severity': 1
 			},
-		'6': {'short_description': 'Potential Contamination',
+		{'short_description': 'Potential Contamination',
 			'description': 'Foods must be protected from contamination at all times.',
 			'severity': 1
 			},
-		'7': {'short_description': 'Dish Washing Procedures',
+		{'short_description': 'Dish Washing Procedures',
 			'description': 'Proper dish washing procedures must be followed. Mechanical washing: dishwashers must be National Sanitation Foundation (NSF) approved or equivalent, designed to wash at 60oC (140oF) and utilize an approved sanitizing agent. Manual washing: (wash/rinse/sanitize in a three-compartment sink): first compartment - clean hot water 44oC (111oF) with detergent; second compartment - clean hot water 44oC (111oF); third compartment - approved sanitizing method.',
 			'severity': 1
 			},
-		'8': {'short_description': 'Food and Water Source',
+		{'short_description': 'Food and Water Source',
 			'description': 'Food, water and ice must be from an approved source and must also be wholesome, free from damage or spoilage and transported under proper temperatures, where applicable.',
 			'severity': 1
 			},
-		'9': {'short_description': 'placeholder for 9', 
+		{'short_description': 'placeholder for 9', 
 			'description': 'place holder for 9',
 			'severity': 1
 			},
-		'10': {'short_description': 'Food Storage', 
+		 {'short_description': 'Food Storage', 
 			'description': 'Food must be protected from contamination during storage, preparation, display, service and transport. No food is to be stored on the floor unless it is in an approved container. The lowest shelf is to be high enough to allow easy cleaning of the floor.',
 			'severity': 1
 			},
-		'11': {'short_description': 'Thermometer Access',
+		{'short_description': 'Thermometer Access',
 			'description': 'An accurate, metal-stemmed (food-grade) probe thermometer must be available to monitor temperatures of potentially hazardous foods.',
 			'severity': 1
 			},
-		'12': {'short_description': 'Dish Washing Facilities',
+		{'short_description': 'Dish Washing Facilities',
 			'description': 'Approved dish washing facilities must be installed and properly maintained. An adequate supply of cleaning supplies, chemicals, etc. must be available at all times. \'Clean-in-place\' equipment must be washed and sanitized according to manufacturers instructions.',
 			'severity': 1
 			},
-		'13': {'short_description': 'Garbage Containers',
+		{'short_description': 'Garbage Containers',
 			'description': 'An adequate number of approved, covered garbage containers must be provided at all food preparation areas. Containers are to be kept clean and the contents removed at least daily. Garbage storage must be of an approved design with a lid that seals. It must be kept clean and free of vermin and serviced as required.',
 			'severity': 1
 			},
-		'14': {'short_description': 'Vermin',
+		{'short_description': 'Vermin',
 			'description': 'All restaurants are to be free of vermin',
 			'severity': 1
 			},
-		'15': {'short_description': 'Building Conditions',
+		{'short_description': 'Building Conditions',
 			'description': 'Floors, walls and ceilings of all rooms in which food is stored, prepared or served or in which dishes, utensils and equipment are washed or stored should be kept clean and in good repair.',
 			'severity': 1
 			},
-		'16': {'short_description': 'Building Conditions',
+		{'short_description': 'Building Conditions',
 			'description': 'Approved plumbing must be installed and properly maintained to prevent food contamination.  Light shields or shatterproof bulbs are to be provided in every room in which food is prepared or stored. Unless otherwise approved, every restaurant is to have a ventilation system that prevents the accumulation of odours, smoke, grease/oils and condensation.',
 			'severity': 1
 			}
-		}
+		]
 
 	for i, item in items:
 		try:
-			dbItem = HealthInspectionItem.objects.get(number=i)
+			dbItem = HealthInspectionItem.objects.get(number=i+1)
 		except HealthInspectionItem.DoesNotExist:	
 			dbItem = HealthInspectionItem()
-			dbItem.number =i
-			dbItem.severity = item['severity']
+			dbItem.number =i+1
+			dbItem.severity = int(item['severity'])
 		dbItem.description = item['description']
 		dbItem.short_description = item['short_description']
 		dbItem.save()
