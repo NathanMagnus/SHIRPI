fba44ebd58a2edb2c4502af76eed96837f20afbe
Nathan <ddragonsdd@gmail.com>
2009-11-08 19:54:17 -0500

    splitting up views.py

--- urls.py
+++ urls.py
@@ -19,16 +19,17 @@ urlpatterns = patterns('',
     (r'^cs215/shirpi/browse/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?$', 'project.SHIRPI.views.browse'),
     (r'^cs215/shirpi/view_profile/(?P<user_name>[^/]+)/?$', 'project.SHIRPI.views.view_profile'),
     
-    (r'^cs215/shirpi/add_favourite/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?', 'project.SHIRPI.views.add_favourite'),
-    (r'^cs215/shirpi/view_favourites/(?P<user_name>[^/]+)/?$', 'project.SHIRPI.views.view_favourites'),
-    (r'^cs215/shirpi/edit_favourites/?$', 'project.SHIRPI.views.edit_favourites'),
-    (r'^cs215/shirpi/delete_favourite/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?', 'project.SHIRPI.views.delete_favourite'),
+    (r'^cs215/shirpi/add_favourite/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?', 'project.SHIRPI.favourite_views.add_favourite'),
+    (r'^cs215/shirpi/view_favourites/(?P<user_name>[^/]+)/?$', 'project.SHIRPI.favourite_views.view_favourites'),
+    (r'^cs215/shirpi/edit_favourites/?$', 'project.SHIRPI.favourite_views.edit_favourites'),
+    (r'^cs215/shirpi/delete_favourite/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?', 'project.SHIRPI.favourite_views.delete_favourite'),
     
     (r'^cs215/shirpi/comment/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?', 'project.SHIRPI.views.comment'),
     (r'^cs215/shirpi/edit_comment/(?P<comment_id>\d+)/$', 'project.SHIRPI.views.edit_comment'),
     (r'^cs215/shirpi/view_comments/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?$', 'project.SHIRPI.views.view_comments'),
     (r'^cs215/shirpi/save/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?', 'project.SHIRPI.views.save'),
     (r'^cs215/shirpi/save/(?P<comment_id>\d+)/?', 'project.SHIRPI.views.save_edit'),
+#    (r'^cs215/shirpi/edit_profile/?', 'project.SHIRPI.view
     
     (r'^cs215/shirpi/accounts/?', include('registration.urls')),
     (r'^cs215/SHIRPI|shirpi/?', 'project.SHIRPI.views.index') # not catch-all
