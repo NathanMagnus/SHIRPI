6671f19eefec8b47afb256218e00a4f6f75d50be
Nathan <ddragonsdd@gmail.com>
2009-11-16 22:39:20 -0500

    rewrite comment form - function2

--- SHIRPI/forms.py
+++ SHIRPI/forms.py
@@ -7,10 +7,10 @@ from django.forms.widgets import HiddenInput, Textarea
 
 class CommentForm(ModelForm):
 	comment = forms.CharField(widget=Textarea())
-	cleanliness = forms.IntegerField(widget=HiddenInput)
-	food_quality = forms.IntegerField(widget=HiddenInput)
-	atmosphere = forms.IntegerField(widget=HiddenInput)
-	wait_time = forms.IntegerField(widget=HiddenInput)
+	cleanliness = forms.IntegerField()
+	food_quality = forms.IntegerField()
+	atmosphere = forms.IntegerField()
+	wait_time = forms.IntegerField()
 	class Meta:
 		model = Comment
 		exclude = ('restaurant', 'author', 'id', 'combined', 'created', 'last_modified')
