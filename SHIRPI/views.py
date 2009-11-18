ee54077209e8288728c7f50e3e686afe64c05f47
Nathan <ddragonsdd@gmail.com>
2009-11-18 13:32:59 -0500

    url fix

--- SHIRPI/views.py
+++ SHIRPI/views.py
@@ -67,7 +67,7 @@ def browse(request, restaurant_name = None, restaurant_address = None, api_flag 
 def view_restaurant(request, restaurant_name, restaurant_address):
 	try:
 		restaurant = Restaurant.objects.get(name__iexact=urllib.unquote_plus(restaurant_name), address__iexact=urllib.unquote_plus(restaurant_address))
-		return render_to_response("SHIRPI/view_restaurant.html", {'restaurant': restaurant}, RequestContest(request))
+		return render_to_response("SHIRPI/view_restaurant.html", {'restaurant': restaurant}, RequestContext(request))
 	except Restaurant.DoesNotExist:
 		return HttpResponseRedirect("/cs215/SHIRPI/browse.html")
 
