595e8aaf42d769845812a60932c99048aa83028a
Nathan <ddragonsdd@gmail.com>
2009-11-18 15:23:35 -0500

    ranges

--- SHIRPI/templatetags/comment_filters.py
+++ SHIRPI/templatetags/comment_filters.py
@@ -14,7 +14,8 @@ def display_comment(value, user,autoescape=None):
 	result = "<h3 class='timestamp'>" 
 	result += "<a href='#" + urllib.quote_plus(str(value.id)) + "' name ='" + urllib.quote_plus(str(value.id)) + "'>" + str(value.last_modified.ctime()) + "</a>"	#Anchor position can be better set when layout is more mature
 	result += "</h3>\n"
-	result += "<h4 class='delete'><a href='/cs215/shirpi/delete_comment/" + str(value.id) + "'>Delete</a></h4>"
+	if user.username == value.author.username or user.has_perm("SHIRPI.comment"):
+		result += "<h4 class='delete'><a href='/cs215/shirpi/delete_comment/" + str(value.id) + "'>Delete</a></h4>"
 	result += "<h3 class='username'>"
 	if value.author.username != "Anonymous":
 	        result += "<a href='/cs215/shirpi/view_profile/" + urllib.quote_plus(value.author.username) + "/'>"
--- SHIRPI/views.py
+++ SHIRPI/views.py
@@ -21,7 +21,7 @@ def index(request):
 	return render_to_response("SHIRPI/index.html", {'Critical':Critical, 'Moderate':Moderate,'Good':Good}, RequestContext(request))
 
 #browsing restaurants
-def browse(request, restaurant_name = None, restaurant_address = None, api_flag = None):
+def browse(request, restaurant_name = None, restaurant_address = None, api_flag = None, range = None):
 	# TODO: Seriously consider exclusively using GETs for input instead of view parameters
 	
 	# Prepare strings for database query
@@ -37,10 +37,16 @@ def browse(request, restaurant_name = None, restaurant_address = None, api_flag 
 		restaurant_name = ""
 	if restaurant_address == "all":
 		restaurant_address = ""
+	if restaurant_name == "good":i
+		range = GOOD_VAL	
+	elif restaurant_name == "critical":
+		range = CRITICAL_VAL
+	elif restaurant_name == "moderate":
+		range = MODERATE_VAL
 	
 	# Query Database
 	try:
-		results = Restaurant.objects.filter(name__icontains=restaurant_name, address__icontains=restaurant_address)
+		results = Restaurant.objects.filter(name__icontains=restaurant_name, address__icontains=restaurant_address, health_report_status=range)
 	except Restaurant.DoesNotExist:
 		error = "No results."
 	
