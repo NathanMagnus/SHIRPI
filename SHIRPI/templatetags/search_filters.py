e937ae1dc31aecf74501d176bd53490b307b268a
Nathan <ddragonsdd@gmail.com>
2009-11-19 12:25:22 -0500

    lower limit and upper limit, time to go to class

--- SHIRPI/templatetags/search_filters.py
+++ SHIRPI/templatetags/search_filters.py
@@ -4,17 +4,17 @@ from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
 
 from project.SHIRPI.settings import *
+from project.SHIRPI.templatetags import sort_filters
 
 register = template.Library()
 
 #display a group of restaurants in a table
 @register.filter
 def display_search_field(page, autoescape=None):
-	result ="<form name='search' id='search_form' method='get'>"
-	result +="	<div id='search'>"
+	result ="	<div id='search'>"
 	result +="	<label for='restaurant_name'>Restaurant Name: </label><input type='text' name='restaurant_name' id='restaurant_name' /><br />"
 	result +="	<label for='restaurant_address'>Restaurant Address: </label><input type='text' name='restaurant_address' id='restaurant_address' /><br />"
+	result +="	<label for='upper_limit'>Upper Limit: </label><input type='text' name='upper_limit' id='upper_limit' />"
+	result +="	<label for='lower_limit'>Lower Limit: </label><input type='text' name='lower_limit' id='lower_limit' />"
 	result +="	</div>"
-	result +="	<input type='submit' name='submitSearch' value='Search' />"
-	result += "</form>"
 	return mark_safe(result)
