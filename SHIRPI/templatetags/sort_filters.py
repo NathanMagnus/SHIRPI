c6b414832a128ee6ca835453f61e8cfc4e0167f4
Nathan <ddragonsdd@gmail.com>
2009-11-19 00:15:20 -0500

    more

--- SHIRPI/templatetags/sort_filters.py
+++ SHIRPI/templatetags/sort_filters.py
@@ -11,18 +11,16 @@ register = template.Library()
 @register.filter
 def display_sort_field(page, autoescape=None):
 	result ="<form name='sort' id='sort_form'>"
-	result +="
-		<div id='sort'>
-		<h4>Sort By</h4>
-		<div class='type'><a href='"+page+"?order_by=combined'>Combined Score</a></div>
-		<div class='type'><a href='"+page+"?order_by=cleanliness'>Cleanliness</a></div>
-		<div class='type'><a href='"+page+"?order_by=food_quality'>Food Quality</a></div>
-		<div class='type'><a href='"+page+"?order_by=atmosphere'>Atmosphere</a></div>
-		<div class='type'><a href='"+page+"?order_by=wait_time'>Wait Time</a></div>
-		<h4>Exclude By</h4>
-		<div class='type'><a href='"+page+"?exclude_by=good'>Good</a></div>
-		<div class='type'><a href='"+page+"?exclude_by=moderate'>Moderate</a></div>
-		<div class='type'><a href='"+page+"?exclude_by=critical'>Critical</a></div>
-		</div>
-	"
+	result +="	<div id='sort'>"
+	result +="	<h4>Sort By</h4>"
+	result +="	<div class='type'><a href='"+page+"?order_by=combined'>Combined Score</a></div>"
+	result +="	<div class='type'><a href='"+page+"?order_by=cleanliness'>Cleanliness</a></div>"
+	result +="	<div class='type'><a href='"+page+"?order_by=food_quality'>Food Quality</a></div>"
+	result +="	<div class='type'><a href='"+page+"?order_by=atmosphere'>Atmosphere</a></div>"
+	result +="	<div class='type'><a href='"+page+"?order_by=wait_time'>Wait Time</a></div>"
+	result +="	<h4>Exclude By</h4>"
+	result +="	<div class='type'><a href='"+page+"?exclude_by=good'>Good</a></div>"
+	result +="	<div class='type'><a href='"+page+"?exclude_by=moderate'>Moderate</a></div>"
+	result +="	<div class='type'><a href='"+page+"?exclude_by=critical'>Critical</a></div>"
+	result +="	</div>"
 	return mark_safe(result)
