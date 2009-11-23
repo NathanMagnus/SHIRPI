c98b27f2687764958c26842e956a1ad2eb836abd
Nathan <ddragonsdd@gmail.com>
2009-11-22 22:26:38 -0500

    remove submit button

--- SHIRPI/templatetags/search_filters.py
+++ SHIRPI/templatetags/search_filters.py
@@ -11,10 +11,10 @@ register = template.Library()
 #display a group of restaurants in a table
 @register.filter
 def display_search_field(page, autoescape=None):
-	result ="	<div id='search'>"
-	result +="	<label for='restaurant_name'>Restaurant Name: </label><input type='text' name='restaurant_name' id='restaurant_name' /><br />"
-	result +="	<label for='restaurant_address'>Restaurant Address: </label><input type='text' name='restaurant_address' id='restaurant_address' /><br />"
-	result +="	<label for='upper_limit'>Upper Limit: </label><input type='text' name='upper_limit' id='upper_limit' />"
-	result +="	<label for='lower_limit'>Lower Limit: </label><input type='text' name='lower_limit' id='lower_limit' />"
-	result +="	</div>"
+	result ="<div id='search'>\n"
+	result +="<input type='text' name='restaurant_name' id='restaurant_name' /><br />\n"
+	result +="<input type='text' name='restaurant_address' id='restaurant_address' /><br />\n"
+	result +="<input type='text' name='upper_limit' id='upper_limit' />\n"
+	result +="<input type='text' name='lower_limit' id='lower_limit' />\n"
+	result +="</div>\n"
 	return mark_safe(result)
