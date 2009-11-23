8717537fb6f98cba4820fd42cfaec30c52b25ba2
kylei <kyle@basementbobs.com>
2009-11-22 19:09:35 -0600

    Separation of url files for more compact expressions. Also removed .pyc files

---
+++ SHIRPI/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls.defaults import *
+import  project.SHIRPI
+
+urlpatterns = patterns('',
+    (r'^browse/?(?P<restaurant_name>[^/]+)?/?(?P<restaurant_address>[^/]+)?/?$', 'project.SHIRPI.views.browse'),
+    (r'^view/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?$', 'project.SHIRPI.views.view_restaurant'),
+    (r'^(?P<api_flag>api)/?(?P<restaurant_name>[^/]+)?/?(?P<restaurant_address>[^/]+)?/?$', 'project.SHIRPI.views.browse'),
+    
+    (r'^view_profile/(?P<user_name>[^/]+)/?$', 'project.SHIRPI.views.view_profile'),
+    (r'^edit_profile/?', 'project.SHIRPI.views.edit_profile'),
+    
+    (r'^add_favourite/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?', 'project.SHIRPI.favourite_views.add_favourite'),
+    (r'^view_favourites/(?P<user_name>[^/]+)/?$', 'project.SHIRPI.favourite_views.view_favourites'),
+    (r'^edit_favourites/?$', 'project.SHIRPI.favourite_views.edit_favourites'),
+    (r'^delete_favourite/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?', 'project.SHIRPI.favourite_views.delete_favourite'),
+    
+    (r'^comment/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?', 'project.SHIRPI.comment_views.comment'),
+    (r'^edit_comment/(?P<comment_id>\d+)/$', 'project.SHIRPI.comment_views.edit_comment'),
+    (r'^view_comments/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?$', 'project.SHIRPI.comment_views.view_comments'),
+    (r'^save/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?', 'project.SHIRPI.comment_views.save'),
+    (r'^save/(?P<comment_id>\d+)/?', 'project.SHIRPI.comment_views.save_edit'),
+    (r'^delete_comment/(?P<comment_id>\d+)/?', 'project.SHIRPI.comment_views.delete_comment'),
+    (r'$', 'project.SHIRPI.views.index')
+)
--- urls.py
+++ urls.py
@@ -14,27 +14,11 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^cs215/admin/', include(admin.site.urls)),
-    (r'^cs215/shirpi/browse/?(?P<restaurant_name>[^/]+)?/?(?P<restaurant_address>[^/]+)?/?$', 'project.SHIRPI.views.browse'),
-    (r'^cs215/shirpi/view/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?$', 'project.SHIRPI.views.view_restaurant'),
-    (r'^cs215/shirpi/(?P<api_flag>api)/?(?P<restaurant_name>[^/]+)?/?(?P<restaurant_address>[^/]+)?/?$', 'project.SHIRPI.views.browse'),
+    (r'^cs215/shirpi/?', include('project.SHIRPI.urls')),
     
-    (r'^cs215/shirpi/view_profile/(?P<user_name>[^/]+)/?$', 'project.SHIRPI.views.view_profile'),
-    (r'^cs215/shirpi/edit_profile/?', 'project.SHIRPI.views.edit_profile'),
-    
-    (r'^cs215/shirpi/add_favourite/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?', 'project.SHIRPI.favourite_views.add_favourite'),
-    (r'^cs215/shirpi/view_favourites/(?P<user_name>[^/]+)/?$', 'project.SHIRPI.favourite_views.view_favourites'),
-    (r'^cs215/shirpi/edit_favourites/?$', 'project.SHIRPI.favourite_views.edit_favourites'),
-    (r'^cs215/shirpi/delete_favourite/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?', 'project.SHIRPI.favourite_views.delete_favourite'),
-    
-    (r'^cs215/shirpi/comment/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?', 'project.SHIRPI.comment_views.comment'),
-    (r'^cs215/shirpi/edit_comment/(?P<comment_id>\d+)/$', 'project.SHIRPI.comment_views.edit_comment'),
-    (r'^cs215/shirpi/view_comments/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?$', 'project.SHIRPI.comment_views.view_comments'),
-    (r'^cs215/shirpi/save/(?P<restaurant_name>[^/]+)/(?P<restaurant_address>[^/]+)/?', 'project.SHIRPI.comment_views.save'),
-    (r'^cs215/shirpi/save/(?P<comment_id>\d+)/?', 'project.SHIRPI.comment_views.save_edit'),
-    (r'^cs215/shirpi/delete_comment/(?P<comment_id>\d+)/?', 'project.SHIRPI.comment_views.delete_comment'),
 
     (r'^cs215/populate/', 'project.populateDB.views.populate'),
 
     (r'^cs215/shirpi/accounts/?', include('registration.urls')),
-    (r'^cs215/SHIRPI|shirpi/?', 'project.SHIRPI.views.index') # not catch-all
+    #(r'^cs215/SHIRPI|shirpi/?', 'project.SHIRPI.views.index') # not catch-all
 )
