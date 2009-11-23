91f9f49cff639ad2e4157e18f9bb772ab0ee02da
Nathan <ddragonsdd@gmail.com>
2009-11-22 22:31:26 -0500

    hidden submit button

--- SHIRPI/templatetags/search_filters.py
+++ SHIRPI/templatetags/search_filters.py
@@ -14,7 +14,7 @@ def display_search_field(page, autoescape=None):
 	result ="<div id='search'>\n"
 	result +="<label for='restaurant_name'>Name:</label><input type='text' name='restaurant_name' id='restaurant_name' />\n"
 	result +="<label for='restaurant_address'>Address:</label><input type='text' name='restaurant_address' id='restaurant_address' /><br />\n"
-	result +="<label for='lower_limit'>Lower Limit::</label><input type='text' name='lower_limit' id='lower_limit' />\n"
+	result +="<label for='lower_limit'>Lower Limit:</label><input type='text' name='lower_limit' id='lower_limit' />\n"
 	result +="<label for='upper_limit'>Upper Limit:</label><input type='text' name='upper_limit' id='upper_limit' />\n"
 	result +="</div>\n"
 	return mark_safe(result)
