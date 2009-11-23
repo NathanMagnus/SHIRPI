c94bb3ca686b2a4b4fb58a84b4f34d460704fdcb
Nathan <ddragonsdd@gmail.com>
2009-11-22 22:42:04 -0500

    initial values

--- SHIRPI/templatetags/search_filters.py
+++ SHIRPI/templatetags/search_filters.py
@@ -10,11 +10,11 @@ register = template.Library()
 
 #display a group of restaurants in a table
 @register.filter
-def display_search_field(page, autoescape=None):
+def display_search_field(request, autoescape=None):
 	result ="<div id='search'>\n"
-	result +="<label for='restaurant_name'>Name:</label><input type='text' name='restaurant_name' id='restaurant_name' />\n"
-	result +="<label for='restaurant_address'>Address:</label><input type='text' name='restaurant_address' id='restaurant_address' /><br />\n"
-	result +="<label for='lower_limit'>Lower Limit:</label><input type='text' name='lower_limit' id='lower_limit' />\n"
-	result +="<label for='upper_limit'>Upper Limit:</label><input type='text' name='upper_limit' id='upper_limit' />\n"
+	result +="<label for='restaurant_name'>Name:</label><input type='text' name='restaurant_name' id='restaurant_name' value='"+ request.GET.get('restaurant_name', "") +"'/>\n"
+	result +="<label for='restaurant_address'>Address:</label><input type='text' name='restaurant_address' id='restaurant_address' value='"+ request.GET.get('restaurant_address', "") +"' /><br />\n"
+	result +="<label for='lower_limit'>Lower Limit:</label><input type='text' name='lower_limit' id='lower_limit' value='" + request.GET.get('lower_limit', "") + "'/>\n"
+	result +="<label for='upper_limit'>Upper Limit:</label><input type='text' name='upper_limit' id='upper_limit' value='" + request.GET.get('upper_limit', "") + "/>\n"
 	result +="</div>\n"
 	return mark_safe(result)
