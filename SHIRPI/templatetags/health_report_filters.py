9dbfd63326f08a08dc82402d89537f5ef0911d7e
Nathan <ddragonsdd@gmail.com>
2009-11-23 11:06:39 -0500

    commenting and cleaning up

--- SHIRPI/templatetags/health_report_filters.py
+++ SHIRPI/templatetags/health_report_filters.py
@@ -2,7 +2,7 @@ from django import template
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
 
-register = template.library()
+register = template.Library()
 
 # display a health report
 @register_filter
