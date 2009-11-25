79fbbefae1aca67ebd5077ff055d513fc2d21884
Nathan <ddragonsdd@gmail.com>
2009-11-25 13:33:18 -0500

    delete favourite changes again

--- SHIRPI/favourite_views.py
+++ SHIRPI/favourite_views.py
@@ -33,8 +33,10 @@ def add_favourite(request, restaurant_name, restaurant_address):
 	try:
 		favourite = Favourite.objects.get(restaurant=restaurant, user__username = request.user.username)
 	except Favourite.DoesNotExist:
-		#get all preexisting favourites for the user
+		# get all preexisting favourites for the user
 		rank_set = Favourite.objects.filter(user__username = request.user.username).aggregate(Max('rank'))
+
+		# determine the rank of this favourite
 		if rank_set['rank__max'] == None:
 			rank = 1 
 		else:
@@ -48,32 +50,40 @@ def add_favourite(request, restaurant_name, restaurant_address):
 	return HttpResponseRedirect('/cs215/shirpi/view_favourites/' + request.user.username + '/')
 
 '''
-Function        : 
-Description     :
-Parameter(s)    : 
-                : 
-Return          : 
+Function        : view_favourites
+Description     : display the users favourites
+Parameter(s)    : request - HttpRequest
+                : user_name - the username of the user whose favourites are to be displayed
+Return          : HttpResponse
 '''
 def view_favourites(request, user_name):
 	user_name = urllib.unquote_plus(user_name)
+
+	# get the user
 	try:
 		user_to_view = User.objects.get(username=user_name)
 	except User.DoesNotExist:
 		return render_to_response('SHIRPI/error.html', {'error': "User does not exist"}, RequestContext(request))
+
+	# get the users favourites
 	favourites = Favourite.objects.filter(user=user_to_view).order_by('rank')
 	
 	return render_to_response('SHIRPI/view_favourites.html', {'favourites':favourites, 'user_to_view': user_to_view}, RequestContext(request))
 
 '''
-Function        : 
-Description     :
-Parameter(s)    : 
-                : 
-Return          : 
+Function        : edit_favourites
+Description     : edit the users favourites
+Parameter(s)    : request - HttpRequest
+Return          : HttpResponse
 '''
 def edit_favourites(request):
+
+	# if the form was posted
 	if request.method=="POST":
+		# get the favourites for the user
 		favourites = Favourite.objects.filter(user__username = request.user.username)
+
+		# for each of these favourites, update the rank according to the form
 		for favourite in favourites:
 			favourite.rank = request.POST[favourite.restaurant.name]
 			favourite.save()
@@ -81,23 +91,28 @@ def edit_favourites(request):
 
 
 '''
-Function        : 
-Description     :
-Parameter(s)    : 
-                : 
-Return          : 
+Function        : delete_favourite
+Description     : delete a favourite from the users favourites
+Parameter(s)    : request - HttpResponse
+                : restaurant_name - the name of the restaurant to be removed from the favourites
+		: restaurant_address - the address of the restaurant to be removed from the favourites
+Return          : HttpResponse
 '''
 def delete_favourite(request, restaurant_name, restaurant_address):
 	restaurant_name = urllib.unquote_plus(restaurant_name)
 	restaurant_address = urllib.unquote_plus(restaurant_address)
 	try:
+		# make sure the favourite exists
 		favourite = Favourite.objects.get(user__username = request.user.username, restaurant__name__iexact = restaurant_name, restaurant__address__iexact = restaurant_address)
+
+		# record its rank and delete it
 		rank = favourite.rank
 		favourite.delete()
+
+		# get the rest of the favourites and update them
 		favourites = Favourite.objects.filter(user__username = request.user.username, rank__gt = rank).order_by('rank')
 		for favourite in favourites:
-			favourite.rank = rank
-			rank = rank + 1
+			favourite.rank = favourite.rank - 1
 			favourite.save()
 	except Favourite.DoesNotExist:
 		pass
