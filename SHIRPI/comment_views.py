e8cb4954d750dc56ccc2363f6d0de1a6165fb05b
Nathan <ddragonsdd@gmail.com>
2009-11-25 11:05:42 -0500

    removed redundancy from comments

--- SHIRPI/comment_views.py
+++ SHIRPI/comment_views.py
@@ -166,27 +166,8 @@ def delete_comment(request, comment_id):
 
 		restaurant = comment.restaurant
 		
-		restaurant.cleanliness = restaurant.cleanliness - comment.cleanliness
-		if comment.cleanliness > 0:
-			restaurant.cleanliness_count = restaurant.cleanliness_count -1
+		update_values(restaurant, comment, -1)
 
-		restaurant.atmosphere = restaurant.atmosphere - comment.atmosphere
-		if comment.atmosphere > 0:
-			restaurant.atmosphere_count = restaurant.atmosphere_count -1
-		
-		restaurant.food_quality = restaurant.food_quality - comment.food_quality
-		if comment.food_quality > 0:
-			restaurant.food_quality_count = restaurant.food_quality_count -1
-		
-		restaurant.wait_time = restaurant.wait_time - comment.wait_time
-		if comment.wait_time > 0:
-			restaurant.wait_time_count = restaurant.wait_time_count -1
-
-		restaurant.combined = restaurant.combined - comment.combined
-		if comment.combined > 0:
-			restaurant.combined_count = restaurant.combined_count -1
-		
-		restaurant.save()
 		comment.delete()
 		return HttpResponseRedirect(request.META['HTTP_REFERER'])
 	except Comment.DoesNotExist:
