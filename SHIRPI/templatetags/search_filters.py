6b0c94bd353d1eee1644f75d6f006f0ff3307bbc
Nathan <ddragonsdd@gmail.com>
2009-11-26 09:58:06 -0500

    search sorting by name as default

--- SHIRPI/templatetags/search_filters.py
+++ SHIRPI/templatetags/search_filters.py
@@ -24,7 +24,7 @@ def display_search_field( request ):
 	result +="<label for='restaurant_address'>Address:</label><input type='text' name='restaurant_address' id='restaurant_address' value='" + request.GET.get('restaurant_address', "") + "' /><br />\n"
 	result +="<label for='lower_limit'>Lower Limit:</label><input type='text' name='lower_limit' id='lower_limit' value='" + request.GET.get('lower_limit', "") + "'/>\n"
 	result +="<label for='upper_limit'>Upper Limit:</label><input type='text' name='upper_limit' id='upper_limit' value='" + request.GET.get('upper_limit', "") + "'/>\n"
-	result +="<input type='hidden' name='sort_by' value='name' />\n"
-	result +="<input type='hidden' name='type' value='ASC' />\n"
+	result +="<input type='hidden' name='sort_by' value='" + request.GET.get('sort_by', 'name') + "' />\n"
+	result +="<input type='hidden' name='type' value='" + request.GET.get('type', 'asc')  + "' />\n"
 	result +="</div>\n"
 	return mark_safe(result)
