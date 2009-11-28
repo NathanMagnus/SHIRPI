39b05732414d61b4b3e55ddb28eb4f14f2b5b426
Nathan <ddragonsdd@gmail.com>
2009-11-28 14:07:32 -0500

    apostrophe issue fixed, need to escape values in the filter

--- SHIRPI/templatetags/search_filters.py
+++ SHIRPI/templatetags/search_filters.py
@@ -1,4 +1,5 @@
 import urllib
+from cgi import escape
 from django import template
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
@@ -20,11 +21,11 @@ def display_search_field( request ):
 	result ="<div id='search'>\n"
 
 	# display label and input for form, populate with current GET values
-	result +="<label for='restaurant_name'>Name:</label><input type='text' name='restaurant_name' id='restaurant_name' value='" + request.GET.get('restaurant_name', "") + "'/>\n"
-	result +="<label for='restaurant_address'>Address:</label><input type='text' name='restaurant_address' id='restaurant_address' value='" + request.GET.get('restaurant_address', "") + "' /><br />\n"
-	result +="<label for='lower_limit'>Lower Limit:</label><input type='text' name='lower_limit' id='lower_limit' value='" + request.GET.get('lower_limit', "") + "'/>\n"
-	result +="<label for='upper_limit'>Upper Limit:</label><input type='text' name='upper_limit' id='upper_limit' value='" + request.GET.get('upper_limit', "") + "'/>\n"
-	result +="<input type='hidden' name='sort_by' value='" + request.GET.get('sort_by', 'name') + "' />\n"
-	result +="<input type='hidden' name='type' value='" + request.GET.get('type', 'asc')  + "' />\n"
+	result +="<label for='restaurant_name'>Name:</label><input type='text' name='restaurant_name' id='restaurant_name' value='" + escape(request.GET.get('restaurant_name', "")) + "'/>\n"
+	result +="<label for='restaurant_address'>Address:</label><input type='text' name='restaurant_address' id='restaurant_address' value='" + escape(request.GET.get('restaurant_address', "")) + "' /><br />\n"
+	result +="<label for='lower_limit'>Lower Limit:</label><input type='text' name='lower_limit' id='lower_limit' value='" + escape(request.GET.get('lower_limit', "")) + "'/>\n"
+	result +="<label for='upper_limit'>Upper Limit:</label><input type='text' name='upper_limit' id='upper_limit' value='" + escape(request.GET.get('upper_limit', "")) + "'/>\n"
+	result +="<input type='hidden' name='sort_by' value='" + escape(request.GET.get('sort_by', 'name')) + "' />\n"
+	result +="<input type='hidden' name='type' value='" + escape(request.GET.get('type', 'asc'))  + "' />\n"
 	result +="</div>\n"
 	return mark_safe(result)
