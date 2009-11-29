5b75556c7e95f0be7174fc916178117dc115195d
kylei <cs215@basementbobs.com>
2009-11-29 07:42:13 -0500

    search integrated. NATHAN LOOK AT THIS COMMIT

--- SHIRPI/templatetags/search_filters.py
+++ SHIRPI/templatetags/search_filters.py
@@ -19,23 +19,45 @@ def display_search_field( request ):
 	# enclosing div
 	result ="<div id='search'>\n"
 
-	# display label and input for form, populate with current GET values
-	result +="<input type='text' name='restaurant_name' id='restaurant_name' value='" + escape(request.GET.get('restaurant_name', "Location Name")) + "' " + \
-		"onblur=\"if (this.value == '') this.value='Location Name';\" onfocus=\"if (this.value == 'Location Name') this.value='';\" " + \
-		"onsubmit=\"if (this.value == 'Location Name') this.value='';\"/>\n"
+	# display label and input for form, populate with current GET values if possible
 	
-	result +="<input type='text' name='restaurant_address' id='restaurant_address' value='" + escape(request.GET.get('restaurant_address', "Street / Address")) + "' " + \
+	try:
+		name = escape(request.GET.get('restaurant_name', 'Location Name'))
+		address = escape(request.GET.get('restaurant_address', 'Street / Address'))
+		lower_limit = escape(request.GET.get('lower_limit', ""))
+		upper_limit = escape(request.GET.get('upper_limit', ""))
+		sort_by = escape(request.GET.get('sort_by', 'name'))
+		sort_direction =  escape(request.GET.get('type', 'asc'))
+	except:
+		name = 'Location Name'
+		address = 'Street / Address'
+		lower_limit = ''
+		upper_limit = ''
+		sort_by = 'name'
+		sort_direction = 'asc'
+	
+	
+	# This is for display purposes. Easier to handle here than with javascript
+	if name == '':
+		name = 'Location Name'
+	if address == '':
+		address = 'Street / Address'
+	
+	
+	result +="<input type='text' name='restaurant_name' id='restaurant_name' value='" + name + "' " + \
+		"onblur=\"if (this.value == '') this.value='Location Name';\" onfocus=\"if (this.value == 'Location Name') this.value='';\"/>\n"
+	
+	result +="<input type='text' name='restaurant_address' id='restaurant_address' value='" + address + "' " + \
 		"onblur=\"if (this.value == '') this.value='Street / Address';\" onfocus=\"if (this.value == 'Street / Address') this.value='';\"/>\n"
 	
-	result +="<input type='text' name='lower_limit' id='lower_limit' value='" + escape(request.GET.get('lower_limit', "")) + "'/>\n"
+	result +="<input type='text' name='lower_limit' id='lower_limit' value='" + lower_limit + "'/>\n"
 	
-	result +="<input type='text' name='upper_limit' id='upper_limit' value='" + escape(request.GET.get('upper_limit', "")) + "'/>\n"
+	result +="<input type='text' name='upper_limit' id='upper_limit' value='" + upper_limit + "'/>\n"
 	
-	result +="<input type='hidden' name='sort_by' value='" + escape(request.GET.get('sort_by', 'name')) + "' />\n"
+	result +="<input type='hidden' name='sort_by' value='" + sort_by + "' />\n"
 	
-	result +="<input type='hidden' name='type' value='" + escape(request.GET.get('type', 'asc'))  + "' />\n"
+	result +="<input type='hidden' name='type' value='" + sort_direction + "' />\n"
 	result +="</div>\n"
 	
-	#<input type="text" size="17" name="searchstr" value="Torrents" onblur="if (this.value == '') this.value='Torrents';" onfocus="if (this.value == 'Torrents') this.value='';" accesskey="t"/>
 	
 	return mark_safe(result)
