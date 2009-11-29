75c8b03370f55827868fd1c950583b8eb105d7ef
kylei <cs215@basementbobs.com>
2009-11-29 06:20:46 -0500

    first portion of heading finished. struggling to integrate searching into base

--- SHIRPI/templatetags/search_filters.py
+++ SHIRPI/templatetags/search_filters.py
@@ -20,11 +20,22 @@ def display_search_field( request ):
 	result ="<div id='search'>\n"
 
 	# display label and input for form, populate with current GET values
-	result +="<label for='restaurant_name'>Name:</label><input type='text' name='restaurant_name' id='restaurant_name' value='" + escape(request.GET.get('restaurant_name', "")) + "'/>\n"
-	result +="<label for='restaurant_address'>Address:</label><input type='text' name='restaurant_address' id='restaurant_address' value='" + escape(request.GET.get('restaurant_address', "")) + "' /><br />\n"
-	result +="<label for='lower_limit'>Lower Limit:</label><input type='text' name='lower_limit' id='lower_limit' value='" + escape(request.GET.get('lower_limit', "")) + "'/>\n"
-	result +="<label for='upper_limit'>Upper Limit:</label><input type='text' name='upper_limit' id='upper_limit' value='" + escape(request.GET.get('upper_limit', "")) + "'/>\n"
+	result +="<input type='text' name='restaurant_name' id='restaurant_name' value='" + escape(request.GET.get('restaurant_name', "Location Name")) + "' " + \
+		"onblur=\"if (this.value == '') this.value='Location Name';\" onfocus=\"if (this.value == 'Location Name') this.value='';\" " + \
+		"onsubmit=\"if (this.value == 'Location Name') this.value='';\"/>\n"
+	
+	result +="<input type='text' name='restaurant_address' id='restaurant_address' value='" + escape(request.GET.get('restaurant_address', "Street / Address")) + "' " + \
+		"onblur=\"if (this.value == '') this.value='Street / Address';\" onfocus=\"if (this.value == 'Street / Address') this.value='';\"/>\n"
+	
+	result +="<input type='text' name='lower_limit' id='lower_limit' value='" + escape(request.GET.get('lower_limit', "")) + "'/>\n"
+	
+	result +="<input type='text' name='upper_limit' id='upper_limit' value='" + escape(request.GET.get('upper_limit', "")) + "'/>\n"
+	
 	result +="<input type='hidden' name='sort_by' value='" + escape(request.GET.get('sort_by', 'name')) + "' />\n"
+	
 	result +="<input type='hidden' name='type' value='" + escape(request.GET.get('type', 'asc'))  + "' />\n"
 	result +="</div>\n"
+	
+	#<input type="text" size="17" name="searchstr" value="Torrents" onblur="if (this.value == '') this.value='Torrents';" onfocus="if (this.value == 'Torrents') this.value='';" accesskey="t"/>
+	
 	return mark_safe(result)
