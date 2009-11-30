32873c254385b3de6c5ace9f6e9efd656352cfd0
Nathan <ddragonsdd@gmail.com>
2009-11-30 10:43:26 -0500

    forgot include

--- SHIRPI/templatetags/comment_filters.py
+++ SHIRPI/templatetags/comment_filters.py
@@ -1,7 +1,7 @@
 import urllib
 from datetime import datetime, date
 from django import template
-from django.utils.html import conditional_escape
+from django.utils.html import conditional_escape, escape
 from django.utils.safestring import mark_safe
 
 register = template.Library()
