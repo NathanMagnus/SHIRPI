36404e2de5ebce6f86eb68ca1f93c2de3bb490ec
kylei <cs215@basementbobs.com>
2009-11-30 09:34:42 -0500

    template setting

--- settings.py
+++ settings.py
@@ -73,7 +73,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/user1/src/project/templates',
+    '/hom/cs215/project/templates',
 )
 
 INSTALLED_APPS = (
